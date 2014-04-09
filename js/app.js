a0d56c7ec23fdb9afe709c047e85bdb0a0303e38
Full Metal <tyler9725@gmail.com>
2014-04-08 19:23:14 -0500

    durp.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 module = angular.module('Localist',['ngSanitize']);
 
-}));turn marked(data);{(function(){
+module.filter("marked",(function(){
 	return function(data){
 		return marked(data);
 	};
@@ -11,7 +11,7 @@ module.factory("Storage",(function($q){
 	var storage = {
 		mechanism:(function(lawn){
 			var that = this;
-			that.loadCurentPost = (function(){
+			that.loadCurrentPost = (function(){
 				var promise = $q.defer();
 				lawn.exists("currentPost",(function(exists){
 					if(exists){
@@ -22,11 +22,38 @@ module.factory("Storage",(function($q){
 				}));
 				return promise.promise;
 			});
+			that.setCurrentPost = (function(postdata) {
+				lawn.save("currentPost",({
+					postID:postdata.postID
+				}));
+			});
+			that.loadPost = (function(postID){
+				var promise = $q.defer();
+				lawn.get("post-"+postID,(function(data){
+					if(data)
+						promise.resolve(data);
+					else
+						promise.reject(null);
+				}));
+				return promise.promise;
+			});
+			that.savePost = (function(postData){
+				var promise = $q.promise();
+				postData.modified=Date.now();
+				lawn.save("post-"+postID,postData,(function(){
+					promise.resolve(postData);
+				}));
+				return promise.promise;
+			});
+			that.createPost = (function(postData){
+				postData.postID = int(Math.floor(postData.created=Date.now()));
+				return that.savePost(postData);
+			})
 			return that;
 		}),
 		prepare:(function(){
 			var promise = $q.defer();
-			lawnchair({
+			Lawnchair({
 				name:"Localist"
 			},(function(lawn){
 				promise.resolve(new storage.mechanism(lawn));
@@ -39,16 +66,35 @@ module.factory("Storage",(function($q){
 
 module.controller('Editor',(function($scope,Storage){
 	$scope.readyState = 0;
-	$scope.postName = null;
-	$scope.markdown = "";
+	$scope.post = {postID:null,markDown:"",name:null};
+	$scope.savePost = (function(){
+		console.log("HAHHH???");
+		Storage.prepare().then(function(mechanism){
+			console.log("Saving...");
+			if($scope.post.postID){
+				// update an existing post.
+				mechanism.savePost($scope.post).then(function(post){
+					console.log("Updated:",post);
+					mechanism.setCurrentPost(post);
+				});
+			} else {
+				// create a new post.
+				mechanism.create($scope.post).then(function(post){
+					console.log("Created:",post);
+					mechanism.setCurrentPost(post);
+				});
+			}
+		});
+	});
 	Storage.prepare().then(function(mechanism){
-		mechanism.loadCurrentPost().then(function(post){
-			if(post===null)
+		mechanism.loadCurrentPost().then(function(currentPost){
+			if(currentPost===null) {
 				$scope.readyState = 1;
-			else {
-				$scope.markDown = post.markDown;
-				$scope.postName = post.name;
-				$scope.readyState = 1;
+			} else {
+				mechanism.loadPost(currentPost.postID).then((function (post) {
+					$scope.post = post;
+					$scope.readyState = 1;
+				}));
 			}
 		});
 	});
